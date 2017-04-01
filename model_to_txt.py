bc3a7eb08b7bdab84ca51f6d84dba02a34d65cff
Thomas Matlak <tmatlak18@wooster.edu>
2017-04-01 14:46:39 -0400

    can extract model to txt

---
+++ model_to_txt.py
@@ -0,0 +1,8 @@
+from gensim.models import word2vec
+import sys
+
+input_f  = sys.argv[1]
+output_f = sys.argv[2]
+
+model = word2vec.Word2Vec.load(input_f)
+model.wv.save_word2vec_format(output_f, binary=False)
